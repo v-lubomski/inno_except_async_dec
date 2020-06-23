f391570bf5cf6d5c65c42ec6522b2f7a498e7b64
Vladislav <vlad.lubomski@gmail.com>
2020-06-23 17:24:57 +0400

    Fix for flake8 and pep257

--- Excercise2/locustfile.py
+++ Excercise2/locustfile.py
@@ -1,20 +1,26 @@
+"""Скрипт нагрузочного тестирования."""
+
 from locust import HttpUser, task, between
-from random import randint, choice
+from random import choice
 import json
 import rstr
 
 
 class UserBehavior(HttpUser):
+    """Класс, описывающий поведение пользователя."""
+
     wait_time = between(1, 10)
 
     @task(1)
-    def writing(self):
+    def writing(self) -> None:
+        """Отправка POST-запросов для записи данных в БД."""
         statuses = ["обрабатывается", "выполняется", "доставлено"]
-        data = {"identifier": rstr.xeger(r'^[a-z0-9]{2,5}$'), "status": choice(statuses)}
+        data = {"identifier": rstr.xeger(r'^[a-z0-9]{2,5}$'),
+                "status": choice(statuses)}
         json_data = json.dumps(data, ensure_ascii=False)
         self.client.post("/write", json_data.encode('utf-8'))
 
     @task(5)
-    def reading(self):
+    def reading(self) -> None:
+        """Отправка GET-запросов для чтения данных из БД."""
         self.client.get("/read")
-
--- Excercise2/web_app.py
+++ Excercise2/web_app.py
@@ -1,4 +1,7 @@
+"""Задание 2."""
+
 from aiohttp import web
+from typing import Any
 import aiosqlite
 import asyncio
 from json import load
@@ -8,7 +11,11 @@ with open('schema.json', encoding='utf-8') as file:
     SCHEMA = load(file)
 
 
-async def init_app():
+async def init_app() -> Any:
+    """Инициализация приложения.
+
+    Создание БД/подключения к БД, назначение routs.
+    """
     app = web.Application()
     app["engine"] = await aiosqlite.connect('deliveries.sqlite')
     await create_db(app["engine"])
@@ -16,7 +23,8 @@ async def init_app():
     return app
 
 
-async def create_db(db):
+async def create_db(db: Any) -> None:
+    """Создание БД."""
     await db.execute(
         'CREATE TABLE IF NOT EXISTS statuses('
         'identifier VARCHAR(5) NOT NULL PRIMARY KEY,'
@@ -24,7 +32,8 @@ async def create_db(db):
     await db.commit()
 
 
-async def read_from_db(db):
+async def read_from_db(db: Any) -> str:
+    """Чтение из БД."""
     output = ''
     async with db.execute('SELECT * FROM statuses') as cursor:
         async for row in cursor:
@@ -32,7 +41,8 @@ async def read_from_db(db):
     return output
 
 
-async def write_to_db(db, json_data):
+async def write_to_db(db: Any, json_data: dict) -> None:
+    """Запись в БД."""
     await db.execute(
         'INSERT OR REPLACE INTO statuses(identifier, status) VALUES (?,?)',
         (json_data['identifier'], json_data["status"]))
@@ -43,12 +53,15 @@ routes = web.RouteTableDef()
 
 
 @routes.get('/read')
-async def give_deliveries(request):
-    return web.Response(text=await asyncio.shield(read_from_db(request.app['engine'])))
+async def give_deliveries(request: Any) -> Any:
+    """Обработка GET-запроса, выдача информации о статусах заказов."""
+    return web.Response(
+        text=await asyncio.shield(read_from_db(request.app['engine'])))
 
 
 @routes.post('/write')
-async def get_request(request):
+async def get_request(request: Any) -> Any:
+    """Обработка POST-запросов, и передача статусов заказов на запись в БД."""
     body = await request.json()
     try:
         validate(schema=SCHEMA, instance=body)
